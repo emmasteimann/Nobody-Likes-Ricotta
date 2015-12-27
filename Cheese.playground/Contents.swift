ca64178173b20433d659fd49ed8f710969ffe467
Emma Steimann <emmasteimann@gmail.com>
2015-12-27 01:47:10 -0800

    nobody...

---
+++ Cheese.playground/Contents.swift
@@ -0,0 +1,21 @@
+// Cheeses of many types
+
+class Ricotta {}
+
+struct Cheese<T> {
+    func isItDelicious() -> Bool {
+        if Ricotta.self is T.Type {
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
+let stringCheese = Cheese<String>()
+let booleanCheese = Cheese<Bool>()
+let ricottaCheese = Cheese<Ricotta>()
+
+stringCheese.isItDelicious()
+booleanCheese.isItDelicious()
+ricottaCheese.isItDelicious()
